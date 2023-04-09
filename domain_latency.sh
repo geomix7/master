2450aa230af59ddc7843e98c93523e6b8af486cb
geomix7 <74089346+geomix7@users.noreply.github.com>
2023-04-09 09:00:10 +0300

    check response time for a domain

--- domain_latency.sh
+++ domain_latency.sh
@@ -5,6 +5,6 @@ read -a IPS
 
 for IP in "${IPS[@]}"
 do
-  echo "Testing IP address: $IP"
+  echo "Checking IP address: $IP"
   curl -o /dev/null -s -w 'Establish Connection: %{time_connect}s\nTTFB: %{time_starttransfer}s\nTotal Time: %{time_total}s\n' $IP
 done
